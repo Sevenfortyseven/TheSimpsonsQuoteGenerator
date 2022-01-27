a92fc477c173e35d48deed43927d9e767fe3e210
aleksandre <alekokhubuluri1994@gmail.com>
2022-01-27 22:45:34 +0400

    Initial project setup

---
+++ TheSimpsonsQuoteGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheSimpsonsQuoteGenerator
+//
+//  Created by aleksandre on 27.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
